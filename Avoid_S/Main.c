985363b3dc3704bc7e84c414259cf509ee943dd3
JwangIn <trundle0912@naver.com>
2024-07-03 17:47:14 +0900

    2024-07-03 별 피하기 게임 완성

--- Avoid_S/Main.c
+++ Avoid_S/Main.c
@@ -2,15 +2,42 @@
 #include "Console.h" // 프로젝트 안에 ""안에 있는 파일을 찾아서 복사해라
 #include <stdbool.h>
 #include <time.h>
+#include <conio.h> // console input output
 // main함수에서 사용할 수 있게 키워드를 작성해주세요.
 
 #define MAX 30
+#define BLACK 0
+#define WHITE 15
 
+void GameOver(void)
+{
+	int temp;
+	printf("플레이어가 사망했습니다.\n 게임을 종료하려면 아무 숫자키를 입력 후 엔터를 쳐주세요.\n");
+	scanf_s("%d", &temp); 
+	
+}
+
+void StartMenu(void)
+{
+	while (true)
+	{
+		Clear();
+		GotoXY(10, 9);
+		printf("별 피하기");
+		GotoXY(3, 20);
+		printf("키를 입력하여 시작하시요.");
+
+		if (_kbhit())
+			break;
+	}
+}
 
 int main()
 {
 	// 콘솔창에 게임의 맵의 크기를 만들어 줘야한다.
 
+	SetTitle("별 피하기 _ 박종현");
+	SetColor(WHITE, 0);
 	SetConsoleSize(30,30);
 	SetConsoleCursorVisibility(0);
 
@@ -21,6 +48,11 @@ int main()
 	// 플레이어의 총알 구현(Bullet)
 	// GotoXY 와 printf를 이용해 총알 구현
 
+	StartMenu();
+
+
+	int score = 0;
+
 	int x = 14, y = 28;  // 플레이어의 x,y좌표
 	int bx = 0, by = 0;  // 총알의 x,y 좌표
 	bool bullet = false; // 현재 총알이 생성되지 않았으면 false, 생성됬으면 true
@@ -28,12 +60,19 @@ int main()
 	int	ey[MAX] = {0};  // 별의 x,y 좌표
 	bool enemy[MAX] = { false };  // 몬스터가 시작할때 false, 생성될 때 true
 	srand(time(NULL));
-	
-	
+	bool IsLive = true;
+	int Special = 3;
+	int Life = 3;
 
 	while (1) // 무한 루프
 	{
+		
 		Clear();
+		GotoXY(0, 0);
+		printf("SCORE %d", score);
+		if (IsLive)
+			score++;
+	
 
 		for (int i = 0; i < MAX; i++)
 		{
@@ -42,7 +81,7 @@ int main()
 
 				ex[i] = (rand() % 15) * 2; // rand() 랜덤 함수의 seed 값을 현재 시간에 따라서 변경시켜주는.
 				//(rand() % 15) * 2; // rand() => 0 ~ 25947 랜덤숫자 반환 // 0~ 14 반환해주는 값                   
-				ey[i] = 0;
+				ey[i] = 2;
 
 				enemy[i] = true;
 				break;
@@ -51,43 +90,58 @@ int main()
 
 
 #if true // 플레이어의 이동 조작
-		if (GetAsyncKeyState(VK_LEFT) & 8001) // 왼쪽키를 눌렀을 때
+		if (IsLive)
 		{
-			if (x < 1) x = 1;
-			x--;
-		}
-		else if (GetAsyncKeyState(VK_RIGHT) & 8001)
-		{
-			if (x > 27) x = 27;
-			x++;
-		}
-		else if (GetAsyncKeyState(VK_DOWN) & 8001)
-		{
-			if (y > 28)y = 28;
-			y++;
-		}
-		else if (GetAsyncKeyState(VK_UP) & 8001)
-		{
-			if (y < 1)y = 1;
-			y--;
+			if (GetAsyncKeyState(VK_LEFT) & 8001) // 왼쪽키를 눌렀을 때
+			{
+				if (x < 1) x = 1;
+				x = x - 2;
+			}
+			else if (GetAsyncKeyState(VK_RIGHT) & 8001)
+			{
+				if (x > 27) x = 27;
+				x = x + 2;
+			}
+			else if (GetAsyncKeyState(VK_DOWN) & 8001)
+			{
+				if (y > 28)y = 28;
+				y++;
+			}
+			else if (GetAsyncKeyState(VK_UP) & 8001)
+			{
+				if (y < 2)y = 2;
+				y--;
+			}
 		}
 #endif		
-		GotoXY(x, y);
-		printf("♣");
-
-#if true // 총알
-		if (GetAsyncKeyState(VK_SPACE) & 8001)
+		GotoXY(x, y); // 플레이어가 살아있을 때 현재 플레이어를 출려해주는 코드
+		if (IsLive)
 		{
-			if (bullet != true)  // !bullet 과 bullet == false 같은 의미
-			{
-				bx = x;
-				by = y - 1;
-
-				bullet = true;
-			}
-			
+			printf("♣");
+			SetColor(BLACK, 13);
+		}
+		else
+		{
+			printf("＠");
+			break;
 		}
 		
+
+#if true // 총알
+		if (IsLive)
+		{
+			if (GetAsyncKeyState(VK_SPACE) & 8001)
+			{
+				if (bullet != true)  // !bullet 과 bullet == false 같은 의미
+				{
+					bx = x;
+					by = y - 1;
+
+					bullet = true;
+				}
+
+			}
+		}
 		if (bullet) // 총알이 생성되어 있을 경우에만
 		{
 			by--; // 한 칸위로 움직이고
@@ -120,25 +174,64 @@ int main()
 				enemy = false;
 		}
 #endif
-
-		for (int i = 0; i < MAX; i++) {
-			if (enemy[i])
+		
+		
+			for (int i = 0; i < MAX; i++)
 			{
-				ey[i]++;
-				GotoXY(ex[i], ey[i]);
-				printf("★");
-				if (ey[i] > 28)
-					enemy[i] = false;
+				if (enemy[i])
+				{
+
+
+					GotoXY(ex[i], ey[i]);
+					printf("★");
+					ey[i]++;
+
+					if (bx == ex[i] && by == ey[i] && bullet == true)
+					{
+						bullet = false;
+						enemy[i] = false;
+						score += 100;
+					}
+					if (ey[i] > 28)
+						enemy[i] = false;
+				}
+
+				if (ex[i] == x && ey[i] == y) // 플레이어가 충돌
+				{
+					// 플레이어가 죽었을 때 작동하는 로직
+					Life--;
+					if (Life < 1)
+					{
+						IsLive = false;
+					}
+				}
+
+
+
 			}
-
-		}
 		
-		
+			if (IsLive)
+			{
+				if (Special > 0)
+				{
+					if (GetAsyncKeyState(VK_CONTROL) & 8001)
+					{
+						score += 2000;
+						for (int i = 0; i < MAX; i++)
+						{
+							enemy[i] = false;
+						}
+						Special--;
+					}
+				}
+			}
+			
 
 		
-
 
 		Sleep(100); // Sleep안의 숫자만큼 기다렸다가 실행
 
 	}
+
+	GameOver();
 }
