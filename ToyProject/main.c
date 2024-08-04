d658f2bef72c71c488de84f1d50f5db2847a230b
JwangIn <trundle0912@naver.com>
2024-08-05 02:22:27 +0900

    20240805 TOYGAME 게임메뉴

--- ToyProject/main.c
+++ ToyProject/main.c
@@ -1,36 +1,44 @@
 #include <stdio.h>
 #include "Console.h"
 #include <stdbool.h>
+#include <stdlib.h>
 
 
+#define ROWS 30 // 가로 
+#define COLS 30 // 세로 
 
+char map[ROWS][COLS] = { 0 };
+char mapString[(COLS * (ROWS + 1)) + 1];
 
 void LoadData()
 {
 
 }
 
-
-
-void MenuMove(int* y)
+void Up(int* Y)
 {
-	
-
-	if (GetAsyncKeyState(VK_DOWN) & 8001)
-	{
-		if (y > 23)y = 23;
-		y += 5;
-	}
-	else if (GetAsyncKeyState(VK_UP) & 8001)
-	{
-		if (y < 13)y = 13;
-		y -= 5;
-	}
+	if (*Y < 2)*Y = 2;
+	(*Y)--;
+}
+void Down(int* Y)
+{
+	if (*Y > 27) *Y = 27;
+	(*Y)++;
+}
+void Right(int* X)
+{
+	if (*X > 27) *X = 27;
+	*X = *X + 1;
+}
+void Left(int* X)
+{
+	if (*X < 2) *X = 2;
+	*X = *X - 1;
 }
 
-
-void StartMenu(int* x, int* y)
+void StartMenu(int y)
 {
+	
 	while (true)
 	{
 		Clear();
@@ -41,23 +49,177 @@ void StartMenu(int* x, int* y)
 		GotoXY(18, 23);
 		printf("3. 종 료 ");
 
-		GotoXY(18, 18);
-		void MenuMove(int* y);
+		
+		if (GetAsyncKeyState(VK_DOWN) & 8001)
+		{
 
+			if(y > 18)y = 18;
+			y += 5;
+
+		}
+		else if (GetAsyncKeyState(VK_UP) & 8001)
+		{
+			if (y < 18)y = 18;
+			y -= 5;
+		}
+	
+		GotoXY(13, y);
+		printf("->");
+		
+		if (y == 13 && GetAsyncKeyState(VK_CONTROL) & 8001)
+			break;
+		
+		else if (y == 18 && GetAsyncKeyState(VK_CONTROL) & 8001)
+		{
+			//LoadData;
+		}
+		else if (y == 23 && GetAsyncKeyState(VK_CONTROL) & 8001)
+			exit(0);
+
+		
+		Sleep(100);
 	}
+	
 }
 
+void MakeMap(char Wall, char(*map)[ROWS]) // map -> 30 * 30
+{
+	for (int i = 0; i < COLS; ++i)
+	{
+		for (int j = 0; j < ROWS; ++j)
+		{
+			map[i][j] = ' ';
+		}
+	}
+	for (int i = 0; i < COLS; ++i) // ' ' => 32 '#' => 35
+	{
+		map[i][0] = Wall;
+		map[i][ROWS - 1] = Wall;
+
+	}
+	for (int j = 0; j < ROWS; ++j)
+	{
+		map[0][j] = Wall;
+		map[COLS - 1][j] = Wall;
+	}
+
+}
+
+void RenderMap()  // 만들어진 맵을 그리는 함수
+{
+	int mapIndex = 0;
+
+	for (int i = 0; i < COLS; ++i)
+	{
+		for (int j = 0; j < ROWS; ++j)
+		{
+			mapString[mapIndex] = map[i][j];
+			mapIndex++;                           // ++ 후위 연산자라 계산이 끝나면 +함
+		}
+		mapString[mapIndex++] = '\n';
+	}
+	mapString[mapIndex] = '\0';
+}
+
+void GoToTargetPos(int a, int b, char* s)
+{
+	GotoXY(a, b);
+	printf("%s", s);
+}
+
+void Monster1(int* x,int* y) // 벽 따라다니면서 플레이어한테 총알
+{
+	
+}
+
+void Monster2()	// 벽만들고 못 나가게 만들기
+{
+
+}
+
+void Monster3() // 플레이어한테 돌진 
+{
+	
+}
+
+void Boss()
+{
+
+}
+
+
+void InputProcess(int* X, int* Y) // x 1 ~ 48 y 1 ~ 29
+{
+	if (GetAsyncKeyState(VK_LEFT) & 8001) // 왼쪽키를 눌렀을 때
+	{
+		if (*X < 2) *X = 2;
+		*X = *X - 1;
+	}
+	else if (GetAsyncKeyState(VK_RIGHT) & 8001)
+	{
+		if (*X > 27) *X = 27;
+		*X = *X + 1;
+	}
+	else if (GetAsyncKeyState(VK_DOWN) & 8001)
+	{
+		if (*Y > 27) *Y = 27;
+		(*Y)++;
+	}
+	else if (GetAsyncKeyState(VK_UP) & 8001)
+	{
+		if (*Y < 2)*Y = 2;
+		(*Y)--;
+	}
+
+}
 
 int main()
 {
 	SetConsoleSize(50, 50);
 	SetConsoleCursorVisibility(0);
-	
-	StartMenu();
+
+	int playerX = 15, playerY = 15;
+
+	int StartX = 15, StartY = 18;
+
+	StartMenu(StartY); 
+
+	MakeMap('#', map);
+	RenderMap();
 
 	while (1)
 	{
+		Clear;
+		GoToTargetPos(0, 0, mapString);
 
+		GoToTargetPos(playerX, playerY, "@");
+
+		InputProcess(&playerX, &playerY);
+
+		/*
+		*  1. 몬스터 소환 (가능하면 랜덤으로)
+		*  2. 몬스터 처치 시 상점 등장 (x초후 몬스터 재등장)
+		*  3. x번 반복 후 보스 
+		*  4. 몬스터 등장 수 증가 상점 증가
+		*  5. 1~4를 반복
+		*  6. 스테이지 증가한거 표시 및 저장 로드가능 하게
+		*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+		Sleep(50);
 	}
 
 
